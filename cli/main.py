6d61154f31f06c975206db9cb712b285fedf876e
PriyeshRaiMinfy <priysh.rai@minfytech.com>
2025-07-21 11:32:00 +0530

    adding main.py

--- cli/main.py
+++ cli/main.py
@@ -4,9 +4,11 @@ import click
 from cli.deploy import deploy_command
 from cli.init import init_command
 from cli.config import config_command
-from cli.status import status_command
+# from cli.status import status_command
 # from cli.populate_efs import populate_efs
 from cli.rollback import rollback_command
+from cli.clone import clone_command
+from cli.status import display_command
 
 @click.group()
 def cli():
@@ -18,7 +20,9 @@ cli.add_command(init_command)
 cli.add_command(config_command)
 cli.add_command(deploy_command)
 cli.add_command(rollback_command)
-cli.add_command(status_command)
+# cli.add_command(status_command)
+cli.add_command(display_command)
+cli.add_command(clone_command)
 
 if __name__ == '__main__':
     cli()
